0f8aa9c672758366167cd07824b2ab30b63208ab
Jordan Kniest <mail@jkniest.de>
2023-07-09 18:44:15 +0200

    Add noise scriptable object with preview dinwo (#3)

---
+++ Editor/Noise/NoiseSoEditor.cs
@@ -0,0 +1,47 @@
+﻿using TheRealIronDuck.Runtime.Noise;
+using TheRealIronDuck.Runtime.Noise.Data;
+using UnityEditor;
+using UnityEngine;
+
+namespace TheRealIronDuck.Editor.Noise
+{
+    [CustomEditor(typeof(NoiseSo))]
+    public class NoiseSoEditor : UnityEditor.Editor
+    {
+        public override bool HasPreviewGUI() => true;
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            var noise = (target as NoiseSo)?.noiseData;
+            if (noise == default)
+            {
+                return;
+            }
+            
+            var noiseMap = NoiseGenerator.GenerateNoiseMap(
+                noise,
+                0,
+                Vector2.zero
+            );
+
+            var colors = new Color[noise.size * noise.size];
+            for (var y = 0; y < noise.size; y++)
+            {
+                for (var x = 0; x < noise.size; x++)
+                {
+                    colors[y * noise.size + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+                }
+            }
+            
+            var texture = new Texture2D(noise.size, noise.size);
+
+            texture.SetPixels(colors);
+            texture.filterMode = FilterMode.Point;
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.Apply();
+
+            var textureRect = new Rect(r.x, r.y, r.width, r.height - 60);
+            GUI.DrawTexture(textureRect, texture, ScaleMode.ScaleToFit, true, 1f);
+        }
+    }
+}
