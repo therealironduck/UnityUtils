5e0f00a3487e316dcf86808dda576c1b9771c351
Jordan Kniest <mail@jkniest.de>
2023-08-18 18:50:02 +0200

    Add NetworkClientTransform component

---
+++ Runtime/Network/ClientNetworkTransform.cs
@@ -0,0 +1,11 @@
+﻿#if THEREALIRONDUCK_NETWORKING
+using Unity.Netcode.Components;
+
+namespace TheRealIronDuck.Runtime.Network
+{
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        protected override bool OnIsServerAuthoritative() => false; 
+    } 
+}
+#endif
