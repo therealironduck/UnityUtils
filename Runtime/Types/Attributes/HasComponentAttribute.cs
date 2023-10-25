fb678bbb30a634e7fead73f73ea3b0bb42f8750f
Jordan Kniest <mail@jkniest.de>
2023-10-25 21:16:24 +0200

    Add HasComponent attribute (#18)

---
+++ Runtime/Types/Attributes/HasComponentAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TheRealIronDuck.Runtime.Types.Attributes
+{
+    public class HasComponentAttribute : Attribute
+    {
+        public readonly Type Type;
+        
+        public HasComponentAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
