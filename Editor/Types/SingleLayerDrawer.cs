dcd0ad88ecd129882e8e3d73abf995c3a6fd8bda
Jordan Kniest <mail@jkniest.de>
2026-05-17 19:33:40 +0200

    Add single layer type

---
+++ Editor/Types/SingleLayerDrawer.cs
@@ -0,0 +1,23 @@
+using TheRealIronDuck.Runtime.Types;
+using UnityEditor;
+using UnityEngine;
+
+namespace TheRealIronDuck.Editor.Types
+{
+    [CustomPropertyDrawer(typeof(SingleLayer))]
+    public class SingleLayerDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, GUIContent.none, property);
+            var layerIndex = property.FindPropertyRelative("layerIndex");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+            if (layerIndex != null)
+            {
+                layerIndex.intValue = EditorGUI.LayerField(position, layerIndex.intValue);
+            }
+            EditorGUI.EndProperty();
+        }
+    }
+}
